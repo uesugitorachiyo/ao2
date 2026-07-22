70c5c164b887bd2bc1084fb033ba2437b456bab3
uesugitorachiyo <uesugitorachiyo@gmail.com>
2026-07-22 06:58:37 -0700

    Split workbench greenfield CLI tests

--- scripts/ao2_windows_outbound_worker.py
+++ scripts/ao2_windows_outbound_worker.py
@@ -623,13 +623,25 @@ AO2_FULL_WINDOWS_PROFILE: tuple[ProfileCommand, ...] = (
             "cli_workbench_launch",
         ),
     },
+    {
+        "name": "test-cli-workbench-greenfield",
+        "argv": (
+            "cargo",
+            "test",
+            "-p",
+            "ao2-cli",
+            "--target-dir",
+            "{ao2-full-target-dir}",
+            "--test",
+            "cli_workbench_greenfield",
+        ),
+    },
     *ao2_cli_approval_profile_commands(
         "test-cli-approval-workbench-core",
         (
             "cli_workbench_evidence",
             "cli_workbench_export",
             "cli_workbench_factory",
-            "cli_workbench_greenfield",
             "cli_workbench_obligation",
         ),
     ),
